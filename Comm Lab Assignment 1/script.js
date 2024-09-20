71c4a6f6e0c63f3612954e4a9d5a80e3ead7d08d
sihyunlkim <sk11019@nyu.edu>
2024-09-20 07:21:30 +0400

    Comm Lab Assignment 1

---
+++ Comm Lab Assignment 1/script.js
@@ -0,0 +1,12 @@
+
+// Function to show the modal
+function showModal(modalId) {
+    document.getElementById(modalId).style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = 'none';
+}
+
+
